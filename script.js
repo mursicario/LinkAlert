55adce8314de99ffefa5537dc30b545495515618
mursicario <cri.haruta@gmail.com>
2025-07-23 19:25:24 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-async function checkLink() {
-  const input = document.getElementById("urlInput").value;
-
-  const res = await fetch(`/api/check?url=${encodeURIComponent(input)}`);
-  const data = await res.json();
-
-  document.getElementById("result").innerText = data.message;
-}
-
